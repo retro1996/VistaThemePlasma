b8dbba3728de62034b0539de31018327322e49b0
furkrn <furkanenesgame@tutanota.com>
2025-03-04 23:27:06 +0300

    Added boot manager screen, reboot screen

--- misc/plymouth/PlymouthVista/install.sh
+++ misc/plymouth/PlymouthVista/install.sh
@@ -1,6 +1,15 @@
 # IMPORTANT: This install script has only been tested on Arch Linux.
 # Installation procedure may vary from distribution-to-distribution.
 
+if [[ "$EUID" != 0 ]]
+then
+    echo "You need to run this script as root."
+    exit 2
+fi
+
+
+cp ./lucon_disable_anti_aliasing.conf /etc/fonts/conf.d/10-lucon_disable_anti_aliasing.conf
+
 rm -rf /usr/share/plymouth/themes/PlymouthVista
 cp -r $(pwd) /usr/share/plymouth/themes/PlymouthVista
 plymouth-set-default-theme -R PlymouthVista
