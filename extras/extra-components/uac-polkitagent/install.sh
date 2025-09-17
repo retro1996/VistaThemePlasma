a707d75bca514b87340e5935073e23516103512c
catpswin56 <catpswin5@proton.me>
2025-09-17 19:46:57 -0300

    Update UAC Polkit Agent installation script

--- extras/extra-components/uac-polkitagent/install.sh
+++ extras/extra-components/uac-polkitagent/install.sh
@@ -6,6 +6,17 @@ VERSION="${array[1]}"
 URL="https://invent.kde.org/plasma/polkit-kde-agent-1/-/archive/master/polkit-kde-agent-1-master.tar.gz"
 ARCHIVE="polkit-kde-agent-1-master.tar.gz"
 SRCDIR="polkit-kde-agent-1-master"
+USE_NINJA=
+
+if [[ "$*" == *"--ninja"* ]]
+then
+    if [[ -z "$(command -v ninja)" ]]; then
+        echo "Attempted to build using Ninja, but Ninja was not found on the system. Falling back to GNU Make."
+    else
+        echo "Compiling using Ninja"
+        USE_NINJA="-G Ninja"
+    fi
+fi
 
 INSTALLDST="/usr/lib/x86_64-linux-gnu/polkit-kde-authentication-agent-1"
 
@@ -26,8 +37,9 @@ cp -r patches/* ./build/$SRCDIR/
 cd ./build/$SRCDIR/
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=/usr ..
+cmake -DCMAKE_INSTALL_PREFIX=/usr .. $USE_NINJA
 cmake --build .
+
 echo "Installing..."
 systemctl --user stop plasma-polkit-agent
 kwriteconfig6 --file ~/.config/plasmanotifyrc --group Services --group policykit1-kde --key ShowPopups --notify --type bool false # Disable notification popup
