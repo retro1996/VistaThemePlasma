53281cb3ed9a2e453b42ba57cf0b9d71ed3f709f
catpswin56 <catpswin5@proton.me>
2026-01-02 19:34:09 -0300

    Attempt to fix the SDDM entries installer script for openSUSE

--- plasma/sddm/login-sessions/install.sh
+++ plasma/sddm/login-sessions/install.sh
@@ -7,7 +7,7 @@ LIBEXEC_PATH="-DCMAKE_INSTALL_LIBEXECDIR=lib"
 if [[ -z "$(command -v pacman)" ]]; then
     LIBEXEC_PATH=
 fi
-if [[ "$(command -v dnf)" ]]; then
+if [[ "$(command -v dnf)" || "$(command -v zypper)" ]]; then
     LIBEXEC_PATH="-DCMAKE_INSTALL_LIBEXECDIR=libexec"
 fi
 
